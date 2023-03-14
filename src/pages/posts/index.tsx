9090bc90f933fd82e3105ad0566f2eaa4526b63f
Trần Ngọc Nhật Vy <21520536@gm.uit.edu.vn>
2023-03-14 20:42:04 +0700

    change change change vid 2.1

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface PostListPageProps {
+}
+
+export default function PostListPage (props: PostListPageProps) {
+  return (
+    <div>
+      Post List Page
+    </div>
+  );
+}
