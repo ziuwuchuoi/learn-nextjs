34e303bd50b8522079ec49291ebd42d406b0f1de
Trần Ngọc Nhật Vy <21520536@gm.uit.edu.vn>
2023-03-14 21:16:33 +0700

    vid 2.2

---
+++ src/pages/posts/[...params].tsx
@@ -0,0 +1,19 @@
+import { useRouter } from 'next/router';
+import * as React from 'react';
+
+export interface ParamsPageProps {
+}
+
+export default function ParamsPage (props: ParamsPageProps) {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>
+      Params Page
+      </h1>
+      <p>
+        Query: {JSON.stringify(router.query)}
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/posts/create.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface CreatePostPageProps {
+}
+
+export default function CreatePostPage (props: CreatePostPageProps) {
+  return (
+    <div>
+      Create Post Page
+    </div>
+  );
+}
