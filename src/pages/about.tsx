abee5f8ea3ccb184fda868b6c678fdbb0678d954
Trần Ngọc Nhật Vy <21520536@gm.uit.edu.vn>
2023-03-15 22:00:18 +0700

    vid2.5

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,21 @@
+import { useRouter } from 'next/router';
 import * as React from 'react';
 
 export interface AboutPageProps {
 }
 
 export default function AboutPage (props: AboutPageProps) {
+  const route = useRouter();
+  console.log('About query', route.query);
   return (
     <div>
       About Page
     </div>
   );
+}
+
+export function getServerSideProps() {
+  return {
+    props: {}
+  }
 }
